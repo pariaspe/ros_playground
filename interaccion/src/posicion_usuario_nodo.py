0e8a98f48a7c332a9fe3c0abccddf8721667d974
Ivnrg <ivanrgrh98@gmail.com>
2020-12-16 19:51:39 +0100

    Creado nodo posicion usuario

---
+++ interaccion/src/posicion_usuario_nodo.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from pos_usuario.msg import pos_usuario
+
+RATE = 10
+
+def shutdown():
+    print(" ")
+    rospy.loginfo("Bye!")
+
+def pos_usuario():
+
+    pub = rospy.Publisher('pos_usuario_topic', pos_usuario, queue_size=10)
+    rospy.init_node('posicion_usuario_nodo', anonymous=True) #nodo
+    rospy.on_shutdown(shutdown)
+    rate = rospy.Rate(RATE) # Frecuencia a 10hz
+
+    while not rospy.is_shutdown():
+    	try:
+    		x_pos = int(raw_input("Posicion en x: "))
+    		y_pos = int(raw_input("Posicion en y: "))
+    		z_pos = int(raw_input("Posicion en z: "))
+    	except EOFError:
+    	# shutdown
+    		break
+    	msg = pos_usuario(x = x_pos, y = y_pos, z = z_pos)
+    	rospy.loginfo(msg)
+    	pub.publish(msg)
+    	rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        posicion_usuario_nodo()
+    except rospy.ROSInterruptException:
+        pass
