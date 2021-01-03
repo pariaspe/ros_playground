9c76dbd3fe1eaccb067394db92bc036d30c835ee
pariaspe <pedroariasperez96@gmail.com>
2021-01-03 13:53:08 +0100

    Added pubs and subs to dialogo_nodo to support the new reloj_nodo

--- interaccion/src/dialogo_nodo.py
+++ interaccion/src/dialogo_nodo.py
@@ -2,17 +2,32 @@
 
 import rospy
 from interaccion.msg import usuario
-from std_msgs.msg import String
-#from interaccion.srv import multiplicador
+from std_msgs.msg import String, Bool
+#serv#from interaccion.srv import multiplicador
+
+
+## Cuidado al descomentar, hay parte del codigo que sirve para el servicio "multiplicador" y parte del "reloj".
+##    Multiplicador: los comentarios empiezan por #serv#
+##    Reloj: los comentarios empiezan por #timer#
 
 
 class Dialogo():
     def __init__(self):
         self.usuario_msg = usuario()
+        self.started = False
 
+        # Subscriber
         rospy.Subscriber("user_topic", usuario, self.usuario_cb)
 
-        #self.multiplicador = rospy.ServiceProxy("multiplicador", multiplicador)
+        # Client Service (Multiplicador)
+        #serv#self.multiplicador = rospy.ServiceProxy("multiplicador", multiplicador)
+
+        # Publishers (Timer)
+        #timer#self.start_pub = rospy.Publisher('start_topic', String, queue_size=10)
+        #timer#self.reset_pub = rospy.Publisher('reset_topic', String, queue_size=10)
+
+        # Subscriber (Timer)
+        #timer#rospy.Subscriber("still_alive", Bool, self.still_alive_cb)
 
         rospy.init_node("dialogo_node")
         rospy.spin()
@@ -21,8 +36,17 @@ class Dialogo():
         self.usuario_msg = msg
         rospy.loginfo(msg)
 
-        # resp = self.multiplicador(self.usuario_msg.edad)
-        # rospy.loginfo("Multiplicador:", resp)
+        #serv#resp = self.multiplicador(self.usuario_msg.edad)
+        #serv#rospy.loginfo("Multiplicador:", resp)
+
+        #timer#if not self.started:
+        #timer#    self.start_pub.publish("start")
+        #timer#    self.started = True
+        #timer#else:
+        #timer#    self.reset_pub.publish("reset")
+
+    def still_alive_cb(self, msg):
+        rospy.loginfo("Heartbeat received at:", msg)
 
 
 if __name__ == '__main__':
