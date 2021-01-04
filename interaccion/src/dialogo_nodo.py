fe2e13c7fdd078c03f9114c37792dbd9ef19f85b
FranBesq <franciscobesq@gmail.com>
2021-01-04 13:27:15 +0100

    Servicio matematico mas cliente de prueba

--- interaccion/src/dialogo_nodo.py
+++ interaccion/src/dialogo_nodo.py
@@ -3,7 +3,8 @@
 import rospy
 from interaccion.msg import usuario
 from std_msgs.msg import String, Bool
-#serv#from interaccion.srv import multiplicador
+#serv
+from interaccion.srv import multiplicador
 
 import subprocess
 
@@ -20,9 +21,12 @@ class Dialogo():
         # Subscriber
         rospy.Subscriber("user_topic", usuario, self.usuario_cb)
 
-        # Client Service (Multiplicador)
-        #serv#self.multiplicador = rospy.ServiceProxy("multiplicador", multiplicador)
-
+        # Service call (Multiplicador)
+        #serv
+        self.edad_mult = self.multiplicador_edad(self.usuario_msg.infPersonal.edad)
+        # DEBUG 
+        print("La edad multiplicada es:"+str(self.edad))
+         
         # Publishers (Timer)
         #timer#self.start_pub = rospy.Publisher('start_topic', String, queue_size=10)
         #timer#self.reset_pub = rospy.Publisher('reset_topic', String, queue_size=10)
@@ -50,6 +54,16 @@ class Dialogo():
 
         subprocess.Popen(["espeak", "-v", "es", sentence])
 
+    # Serv
+    # Devuelve el resultado de llamar al servicio multiplicador
+    def multiplicador_edad(edad):
+    rospy.wait_for_service('multiplicador')
+    try:
+        self.multiplicar_edad = rospy.ServiceProxy('multiplicador', multiplicador)
+        resp1 = self.multiplicar_edad(edad)
+        return resp1.resultado
+    except rospy.ServiceException as e:
+        print("Service call failed: %s"%e)
 
     def still_alive_cb(self, msg):
         rospy.loginfo("Heartbeat received at:", msg)
---
+++ interaccion/src/matematico_nodo.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+from interaccion.srv import multiplicador
+import rospy
+
+def multiplicar_edad(req):
+    resultado = req.MULT * req.edad
+    #Sustituir por ROS_INFO?
+    rospy.loginfo("La edad %s multiplicada por %s es %s ]"%(req.edad, req.MULT, resultado))
+    return resultado
+
+def matematico_nodo():
+    rospy.init_node('matematico_nodo')
+
+    s = rospy.Service('multiplicador', multiplicador, multiplicar_edad)
+    rospy.loginfo("Listo para multiplicar la edad")
+
+    #A la espera de que se solicite el servicio
+    rospy.spin()
+
+if __name__ == "__main__":
+    matematico_nodo()
