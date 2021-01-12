d0202c1cb77324e30bffa6bc7709c9baa46a60d4
FranBesq <franciscobesq@gmail.com>
2021-01-12 20:41:06 +0100

    Nodo reloj añadido - falta testing

---
+++ interaccion/src/reloj_nodo.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String, Bool
+
+class Clock():
+    def _init_(self):
+
+        self.start_flag = False
+        # Recibir topic start_topic de dialogo_nodo para activar el timer
+        rospy.Subscriber("/start_topic", std_msgs/String, self.start_callback)
+        # Recibir topic reset_topic de dialogo_nodo para resetear el timer
+        rospy.Subscriber("/reset_topic", std_msgs/String, self.reset_callback)
+
+        timer_alive  = rospy.timer(rospy.Duration(60), self.still_alive)
+
+        pub = rospy.Publisher("still_alive", Bool, queue_size=10)
+        rospy.init_node("reloj_nodo")
+        rate = rospy.Rate(3) # 3Hz
+
+        while not rospy.is_shutdown():
+            if self.start_flag:
+                self.hora()
+            rate.sleep()
+            rospy.spin()
+
+    # Enviar cada 60 segundos un topic still_alive a dialogo_nodo
+    def still_alive(self):
+        pub.publish(True)
+
+    # Mostrar por pantalla cada 1/3 de segundo la hora local y en UTC
+    # junto a los segundos transcurridos desde el ultimo topic recibido
+    # de dialogo_nodo
+    def hora(self):
+            current = rospy.Time.now()
+            seconds = current - self.last
+
+            rospy.loginfo('The current time is: ' + current)
+            rospy.loginfo('It has been ' + seconds + ' seconds since the last message was received')
+
+    def start_callback(self, msg):
+        self.start_msg = msg
+        rospy.loginfo(msg)
+        self.start_flag = True
+        self.last = rospy.Time.now()
+
+    def reset_callback(self, msg):
+        self.reset_msg = msg
+        rospy.loginfo(msg)
+        self.last = rospy.Time.now()       
+
+        
+
+if __name__ == '__main__':
+    try:
+        Clock()
+    except rospy.ROSInterruptException:
+        pass
