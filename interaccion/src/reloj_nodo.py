f0d4f4a53bf8de91a0bf2979829008b7588f1e9e
pariaspe <pedroariasperez96@gmail.com>
2021-01-19 21:33:36 +0100

    Cleaning code and minor changes

--- interaccion/src/reloj_nodo.py
+++ interaccion/src/reloj_nodo.py
@@ -4,6 +4,7 @@ import rospy
 from std_msgs.msg import String, Bool
 import time
 
+PREFIX = "[CLOCK] "
 LOCAL_TIME = "01:00"
 LAP = 60
 
@@ -11,15 +12,17 @@ class Clock():
     """Nodo Reloj"""
     def __init__(self):
         rospy.init_node("reloj_nodo")
+        rospy.loginfo(PREFIX + "Nodo iniciado")
 
         self.start_flag = False
+
+        # Se crea un contador que llama a la funcion still_alive cada LAP segundos
+        timer_alive = rospy.Timer(rospy.Duration(LAP), self.still_alive)
+
         # Recibir topic start_topic de dialogo_nodo para activar el timer
         rospy.Subscriber("/start_topic", String, self.start_callback)
         # Recibir topic reset_topic de dialogo_nodo para resetear el timer
         rospy.Subscriber("/reset_topic", String, self.reset_callback)
-
-        # Se crea un contador que llama a la funcion still_alive cada LAP segundos
-        timer_alive = rospy.Timer(rospy.Duration(LAP), self.still_alive)
 
         self.pub = rospy.Publisher("still_alive", Bool, queue_size=10)  # topic pub
 
@@ -43,20 +46,20 @@ class Clock():
 
         # Se da formato a los segundos transcurridos desde Epoch
         ttime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(current.secs))
-        rospy.loginfo(ttime + " Z \n" + ttime + "+" + LOCAL_TIME + " (local)")
-        rospy.loginfo('It has been ' + str(seconds) + ' seconds since the last message was received')
+        rospy.loginfo(PREFIX + ttime + " Z \n" + ttime + "+" + LOCAL_TIME + " (local)")
+        rospy.loginfo(PREFIX + 'It has been ' + str(seconds) + ' seconds since the last message was received')
 
     def start_callback(self, msg):
         """Callback que recibe los mensajes del start_topic"""
         self.start_msg = msg
-        rospy.loginfo(msg)
+        rospy.loginfo(PREFIX + str(msg))
         self.start_flag = True  # Flag activado
         self.last = rospy.Time.now()
 
     def reset_callback(self, msg):
         """Callback que recibe los mensajes del reset_topic"""
         self.reset_msg = msg
-        rospy.loginfo(msg)
+        rospy.loginfo(PREFIX + str(msg))
         self.last = rospy.Time.now()
 
 
