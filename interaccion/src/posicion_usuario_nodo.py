15afc68cb46f82947487c7e2d4a1ce8fd33b967c
Francisco Blanco <50753879+FranBesq@users.noreply.github.com>
2021-01-19 17:45:22 +0100

    added posibility of remote publishing for posicion node
    Still needs launch update

--- interaccion/src/posicion_usuario_nodo.py
+++ interaccion/src/posicion_usuario_nodo.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # license removed for brevity
 import rospy
+import sys
 from interaccion.msg import pos_usuario
 
 RATE = 10
+
+is_remote = False
 
 def shutdown():
     print(" ")
@@ -16,20 +19,41 @@ def posicion_usuario():
     rospy.on_shutdown(shutdown)
     rate = rospy.Rate(RATE) # Frecuencia a 10hz
 
-    while not rospy.is_shutdown():
-    	try:
-    		x_pos = int(raw_input("Posicion en x: "))
-    		y_pos = int(raw_input("Posicion en y: "))
-    		z_pos = int(raw_input("Posicion en z: "))
-    	except EOFError:
-    	# shutdown
-    		break
-    	msg = pos_usuario(x = x_pos, y = y_pos, z = z_pos)
-    	rospy.loginfo(msg)
-    	pub.publish(msg)
-    	rate.sleep()
+    rospy.loginfo("Remote is:" + str(is_remote))
+
+    if is_remote: # Remote execution case
+        while not rospy.is_shutdown():
+        	try:
+        		x_pos = 1
+        		y_pos = 1
+        		z_pos = 1
+        	except EOFError:
+        	# shutdown
+        		break
+        	msg = pos_usuario(x = x_pos, y = y_pos, z = z_pos)
+        	rospy.loginfo(msg)
+        	pub.publish(msg)
+        	rate.sleep()
+
+    else: 
+        while not rospy.is_shutdown():
+        	try:
+        		x_pos = int(raw_input("Posicion en x: "))
+        		y_pos = int(raw_input("Posicion en y: "))
+        		z_pos = int(raw_input("Posicion en z: "))
+        	except EOFError:
+        	# shutdown
+        		break
+        	msg = pos_usuario(x = x_pos, y = y_pos, z = z_pos)
+        	rospy.loginfo(msg)
+        	pub.publish(msg)
+        	rate.sleep()
 
 if __name__ == '__main__':
+
+    ##if(len(sys.argv) == 2): # Not very sophisticated arg checking, assumes good usage
+    if sys.argv[-1] == "remote":   
+        remote = True
     try:
         posicion_usuario()
     except rospy.ROSInterruptException:
